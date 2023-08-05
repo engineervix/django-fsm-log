1d88362a8b55966eef12d6959f9fc8756d0c6e8b
Victor Miti <victormiti@umusebo.com>
2023-08-06 00:35:48 +0100

    Run black
    just to trigger GitHub Actions workflow

--- django_fsm_log/migrations/0006_allow_non_numeric_ids.py
+++ django_fsm_log/migrations/0006_allow_non_numeric_ids.py
@@ -2,15 +2,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('django_fsm_log', '0005_description_null'),
+        ("django_fsm_log", "0005_description_null"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='statelog',
-            name='object_id',
+            model_name="statelog",
+            name="object_id",
             field=models.TextField(db_index=True),
         ),
     ]
